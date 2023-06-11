a32a12fe9998c119fa8176118a81f4727c6a5f01
SUSHANT170 <sushant2may@gmail.com>
2023-06-11 15:28:28 +0530

    Updates

---
+++ precache-manifest.a6138c6f3e88a12e2b26dfafe300c62c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c6e8ea3f030c9bad3ac",
+    "url": "/tutedude/static/css/main.b3c5a43a.chunk.css"
+  },
+  {
+    "revision": "3c6e8ea3f030c9bad3ac",
+    "url": "/tutedude/static/js/main.d9b554fd.chunk.js"
+  },
+  {
+    "revision": "2b86e053269d130eb6b5",
+    "url": "/tutedude/static/js/runtime~main.624f3f35.js"
+  },
+  {
+    "revision": "2794150dc615d2ae695b",
+    "url": "/tutedude/static/js/2.067a9909.chunk.js"
+  },
+  {
+    "revision": "2882c4366b4f2990f5cf9302d7f2830c",
+    "url": "/tutedude/static/media/logo_jpg.2882c436.webp"
+  },
+  {
+    "revision": "e373b39ba47eca7c238cbe497bd6dcea",
+    "url": "/tutedude/index.html"
+  }
+];
